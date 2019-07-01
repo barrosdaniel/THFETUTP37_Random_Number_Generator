3c9344ab7955f1e2f6253c965455df8fa88611d9
Daniel Barros <barros.daniel@outlook.com>
2019-07-01 17:39:47 +0800

    feature: Add message paragraph to write message to page

--- random.js
+++ random.js
@@ -3,12 +3,10 @@ var initialNumber = parseInt(prompt('What is the starting number of the range?')
 
 // Ask user for final number
 var endingNumber = parseInt(prompt('What is the finishing number of the range?'));
-console.log(initialNumber);
-console.log(endingNumber);
-
 
 // Calculate random number between 1 and 2
 var randomNumber = Math.floor(Math.random() * (endingNumber - initialNumber + 1)) + initialNumber;
 
 // Display number to page
-document.write('Random number is ' + randomNumber + '.');
+var message = '<p>' + randomNumber + ' is a number between ' + initialNumber + ' and ' + endingNumber + '.</p>';
+document.write(message);
